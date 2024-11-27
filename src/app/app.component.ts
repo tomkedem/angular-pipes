db7d477f4d4c5a779a0f71c5142913a90d16bd21
Tomer Kedem <tompha1@hotmail.com>
2024-11-27 07:36:23 +0200

    Using Built-in Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
+import { DatePipe, DecimalPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [DatePipe, DecimalPipe],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
